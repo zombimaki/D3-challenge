51e2be9bd995e48eb159e7cbfd0c124a85667291
zombimaki <andrewbperez@gmail.com>
2021-01-17 09:27:57 -0600

    circles

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -19,7 +19,7 @@ var height = svgHeight - margin.top - margin.bottom;
 // Create an SVG wrapper, append an SVG group that will hold our chart,
 // and shift the latter by left and top margins.
 var svg = d3
-  .select(".chart")
+  .select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -30,7 +30,54 @@ var chartGroup = svg.append("g")
 
 // Import Data
 d3.csv("assets/data/data.csv").then(function(stateData) {
+
+    // send csv data to console for testing
     console.log(stateData)
+
+    // Step 1: Parse Data/Cast as numbers
+    // ==============================
+    stateData.forEach(function(data) {
+        data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare;        
+      });
+
+    // Step 2: Create scale functions
+    // ==============================
+    var xLinearScale = d3.scaleLinear()
+      .domain([d3.min(stateData, d => d.poverty)-1, d3.max(stateData, d => d.poverty)+5])
+      .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([d3.min(stateData, d => d.healthcare)-1, d3.max(stateData, d => d.healthcare)+1])
+      .range([height, 0]);
+
+    // Step 3: Create axis functions
+    // ==============================
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Step 4: Append Axes to the chart
+    // ==============================
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+    // Step 5: Create Circles
+    // ==============================
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(stateData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", "15")
+    .attr("fill", "green")
+    .attr("opacity", ".5");
+
+
 
 }).catch(function(error) {
     console.log(error);
