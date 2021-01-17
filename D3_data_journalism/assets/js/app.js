df1b87f090bda069b93ddb4cf6bd8fc69ce43ba9
zombimaki <andrewbperez@gmail.com>
2021-01-17 08:42:14 -0600

    d3.csv

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,37 @@
-// @TODO: YOUR CODE HERE!
+//////////////////////////////////////////////////////////////////
+// Create the svg for the scatterplot
+//////////////////////////////////////////////////////////////////
+
+// Setting the dimensions for the SVG container
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+  .select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import Data
+d3.csv("assets/data/data.csv").then(function(stateData) {
+    console.log(stateData)
+
+}).catch(function(error) {
+    console.log(error);
+  });
