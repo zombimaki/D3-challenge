794383400b5d0bfb2e1b941acf68d321726b5f7c
zombimaki <andrewbperez@gmail.com>
2021-01-17 09:45:56 -0600

    bold

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -100,6 +100,22 @@ d3.csv("assets/data/data.csv").then(function(stateData) {
         toolTip.hide(data);
       });
 
+    // Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .attr("font-weight", 700)
+      .text("Lacks Healthcare (%)");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .attr("font-weight", 700)
+      .text("In Poverty (%)");
+
 }).catch(function(error) {
     console.log(error);
   });
